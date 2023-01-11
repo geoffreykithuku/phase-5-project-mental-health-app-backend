cb62d70f300553ac8aa9d250d2e308a6ef94485a
Ms-Waiyego <waiyegobrenda99@gmail.com>
2023-01-11 22:52:52 +0300

    Creating doctor and patient model

---
+++ db/migrate/20230111193758_create_patients.rb
@@ -0,0 +1,11 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
