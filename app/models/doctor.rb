8862519091b3903e4c3ed07fe61848b0a0aded24
Ms-Waiyego <waiyegobrenda99@gmail.com>
2023-01-15 16:41:44 +0300

    doctor validation

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,12 @@
 class Doctor < ApplicationRecord
+    has_many :appointments
+    has_many :doctors,through: :properties
+    has_secure_password
+    validates :name ,presence: true
+    validates :password, confirmation: true
+
+    validates :email,
+        format: { with: /\A(.+)@(.+)\z/, message: "invalid"  },
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 4, maximum: 254 }
 end
