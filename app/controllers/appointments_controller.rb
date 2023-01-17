cf7367d97d8d0046c4fc5b8bf5f3621d175f5266
Ms-Waiyego <waiyegobrenda99@gmail.com>
2023-01-17 13:05:47 +0300

    appointment crud

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,30 @@
 class AppointmentsController < ApplicationController
+    def index 
+        @appointments = Appointment.all 
+        render json: @appointments 
+    end 
+
+    def show 
+        @appointment = Appointment.find_by_id(params[:id]) 
+        render json: @appointment 
+    end 
+
+    def create 
+        appointment = Appointment.new(appointment_time: params["appointment_time"], issue: params["issue"], prescription: params["prescription"], status: params["status"]  , doctor_id: params["doctor_id"], patient_id: params["patient_id"])
+        appointment.save 
+        render json: appointment 
+    end 
+
+    def update
+        appointment = Appointment.find_by(id: params[:id])
+        appointment.update(appointment_time: params["appointment_time"], issue: params["issue"], prescription: params["prescription"], status: params["status"]  , doctor_id: params["doctor_id"], patient_id: params["patient_id"])
+        render json: appointment 
+    end
+
+    def destroy
+        appointment = Appointment.find_by(id: params[:id])
+        appointment.delete
+        render json: appointment 
+    end 
+
 end
