e48bf7099f4aab6b1592e01ff13c0a8ed9f7b9b4
ManyweleMike <mugendimike80@gmail.com>
2023-01-17 12:55:11 +0300

    prescription

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_17_085302) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_095021) do
   create_table "appointments", force: :cascade do |t|
     t.text "issue"
     t.datetime "appointment_time"
