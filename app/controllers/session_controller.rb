0dc3dd4164c2039f3fff35b204fe59d496f95c91
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-01-20 15:08:44 +0300

    fix

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,34 @@
 class SessionController < ApplicationController
     skip_before_action :authorized_account, only: [:login]
+class SessionsController < ApplicationController 
+ wrap_parameters format: []
 
-    def create
-       user = User.find_by(email:params[:email])
-       if user&.authenticate(params[:password])
-           session[user_id:id] = user.id
-           render json: user, status: :created
-       else
-           render json: {error: "Invalid Password or Email"}, status: :unauthorized
-       end
-   end
- 
-   def authorized
-       render json: loggedIn_user
-   end
- 
-   def destroy
-       session.delete :user_id
-       head :no_content  
-   end
+     def create   
+            patient = Patient.find_by(email: params[:email])
+
+            if patient && patient.authenticate(params[:password])
+                session[:user_id] = patient.id
+                render json: patient, status: :created
+            else
+            patient = Patient.new
+                patient.validate
+                render json: {errors: patient.errors.full_messages}, status: :unauthorized
+            end
+      end 
+
+        def destroy
+
+            patient = Patient.find_by(id: session[:user_id])
+
+            if patient
+                session.delete :user_id
+                head :no_content
+            else
+                patient = Patient.new 
+                patient.validate
+                render json: {errors: patient.errors.full_messages}, status: :unauthorized
+            end
+        end
+  
+end
 end
